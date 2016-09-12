d8a07357acd84c268e1b0681576145e122ba8572
Samuel <samueldoogan@gmail.com>
2016-09-12 12:42:56 +0100

    Order comparisons between all accounts and save to json file

--- src/cross_comparison.py
+++ src/cross_comparison.py
@@ -8,13 +8,6 @@ def global_comparison():
     db = json.load(open("../db/top-1000-handles.json"))
     rankings = {}
 
-    # for handle in db.keys():
-    #     rankings[handle] = {}
-    #     for other_handle in db.keys():
-    #         if other_handle not in rankings[handle] and not handle is other_handle:
-    #             rankings[handle][other_handle] = similarity_measure.\
-    #                  difference(db[handle]["Scores"], db[other_handle]["Scores"])
-
     for handle in db.keys():
         rankings[handle] = {}
     for handle, other_handle in itertools.combinations(db.keys(), 2):
@@ -25,16 +18,37 @@ def global_comparison():
     print "writing to file"
     json.dump(rankings, fp, indent=4)
 
+
 def find_most_similar():
     db = json.load(open("../db/comparisons.json"))
-    max = 0
+    most_similar = 0
     for key in db.keys():
         for name, measure in zip(db[key].keys(), db[key].values()):
-            if measure > max:
-                max = measure
+            if measure > most_similar:
+                most_similar = measure
                 pair = name + " : " + key + " ->  " + str(measure)
 
     return pair
 
-print find_most_similar()
 
+def db_as_list():
+    db = json.load(open("../db/comparisons.json"))
+    # save the dictionary format as a list of triples, with
+    # handles as first pair and corresponding score as second
+
+    list_form = []
+
+    for key in db.keys():
+        for name, measure in zip(db[key].keys(), db[key].values()):
+            list_form.append((key, name, measure))
+
+    return list_form
+
+
+def order_by_similarity():
+
+    list_form = db_as_list()
+    list_form.sort(key=lambda x: x[2])
+
+    print "Check"
+    return list_form
