9130c28434ffd3ad7cd02b8c6fa504f4592eb383
doogyb <samueldoogan@gmail.com>
2019-07-31 14:43:18 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='celebritysoulm8',
+      version='0.1',
+      description='Twitter bot which compares twitter uses based on language usage and sentiment analysis',
+      url='https://github.com/doogyb/celebritysoulm8',
+      author='Samuel Doogan',
+      author_email='samueldoogan@gmail.com',
+      packages=['celebritysoulm8'],
+      zip_safe=False)
