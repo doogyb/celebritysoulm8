a28f02b3525b951fb2c6876857aa475cc3497366
doogyb <samueldoogan@gmail.com>
2019-08-01 15:29:38 +0100

    Add exception for chunking

--- celebritysoulm8/twitter_util.py
+++ celebritysoulm8/twitter_util.py
@@ -50,8 +50,7 @@ def auth_twitter():
     consumer_key = open("keys/consumer-key.txt").read().strip()
     consumer_secret = open("keys/consumer-secret.txt").read().strip()
     access_token = open("keys/access-token.txt").read().strip()
-    access_token_secret = open(
-        "keys/access-token-secret.txt").read().strip()
+    access_token_secret = open("keys/access-secret.txt").read().strip()
 
     return consumer_key, consumer_secret, access_token, access_token_secret
 
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import traceback
+
 from celebritysoulm8 import twitterbot
 from celebritysoulm8.twitter_util import *
+from celebritysoulm8.mail import send_email
 
-twitterbot.listen()
+try:
+    twitterbot.listen()
+
+except ChunkedEncodingError as e:
+    chunk += 1
+    send_crash_email(e)
+    if chunk == 3:
+        raise
+
+except Exception as e:
+    send_crash_email(e)
+    raise
