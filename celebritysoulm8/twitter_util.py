9ec8f5a47060acb610a0d3cb443a4c8f3ac6a86f
doogyb <samueldoogan@gmail.com>
2019-09-05 15:55:05 +0100

    TODO: Recode wait time functions

--- celebritysoulm8/twitter_util.py
+++ celebritysoulm8/twitter_util.py
@@ -1,27 +1,4 @@
-from twitter import Twitter
-import time
-import os
 import twitter
-
-
-def wait_time(call):
-    twitter = Twitter(auth=auth_twitter())
-
-    rate_status = twitter.application.rate_limit_status(
-        resources=call.split("/")[0])
-    reset_time = rate_status['resources'][call.split("/")[0]]['/' + call]
-    return int(reset_time['reset'])
-
-
-def block_until_reset(call):
-
-    reset = wait_time(call)
-    while reset - time.time() > 0:
-
-        m, s = divmod(reset - time.time(), 60)
-        os.system('clear')
-        print(("%02d:%02d" % (m, s)))
-        time.sleep(1)
 
 
 def get_url_image_of_user(handle):
